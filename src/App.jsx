9ff9d00525ee31fac958f3f3cb46a7f59d787538
Justen Manni <me@justenmanni.dev>
2023-09-10 13:41:44 +0800

    Initial commit: Set up project structure and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="font-bold">Spring Mart Frontend</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
