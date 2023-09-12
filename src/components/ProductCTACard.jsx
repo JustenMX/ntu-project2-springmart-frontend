d2a59025808f6f625be6e801e6dd15beb0c796f7
Justen Manni <me@justenmanni.dev>
2023-09-12 13:08:26 +0800

    Add new ProductCTACard

--- src/components/ProductCTACard.jsx
+++ src/components/ProductCTACard.jsx
@@ -1,9 +1,58 @@
-//
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
-function ProductCTACard() {
+function ProductCTACard(props) {
+  const {
+    productImgSrc,
+    productImgAlt,
+    productDiscount,
+    productDiscountLabel,
+    productName,
+    productBrand,
+    productCurrentPrice,
+    productOriginalPrice,
+  } = props;
   return (
     <div>
-      <h1>Product Cards</h1>
+      <div
+        className="group relative mb-2 block h-96 overflow-hidden rounded-lg
+        bg-gray-100 shadow-lg lg:mb-3"
+      >
+        <Link to="/springmart/category">
+          <img
+            src={productImgSrc}
+            loading="lazy"
+            alt={productImgAlt}
+            className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+          />
+        </Link>
+        <div className="absolute bottom-2 left-0 flex gap-2">
+          <span className="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">
+            {productDiscount}
+          </span>
+          <span className="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">
+            {productDiscountLabel}
+          </span>
+        </div>
+      </div>
+
+      <div className="flex items-start justify-between gap-2 px-2">
+        <div className="flex flex-col text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl">
+          <Link to="/springmart/category">{productName}</Link>
+          <span className="text-gray-500 text-sm font-light">
+            by {productBrand}
+          </span>
+        </div>
+
+        <div className="flex flex-col items-end">
+          <span className="font-bold text-gray-600 lg:text-lg">
+            {productCurrentPrice}
+          </span>
+          <span className="text-sm text-red-500 line-through">
+            {productOriginalPrice}
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ProductCTAContainer.jsx
+++ src/components/ProductCTAContainer.jsx
@@ -1,3 +1,5 @@
+import ProductCTACard from "./ProductCTACard";
+
 function ProductCTAContainer() {
   return (
     <div className="bg-white py-6 sm:py-8 lg:py-12">
@@ -14,49 +16,17 @@ function ProductCTAContainer() {
         </div>
 
         <div className="grid gap-x-4 gap-y-8 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
-          <div>
-            <a
-              href="#"
-              className="group relative mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
-            >
-              <img
-                src="https://images.unsplash.com/photo-1550258987-190a2d41a8ba?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
-                loading="lazy"
-                alt="Pineapple"
-                className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-              />
+          <ProductCTACard
+            productImgSrc="https://images.unsplash.com/photo-1550258987-190a2d41a8ba?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
+            productImgAlt="Pineapple"
+            productDiscount="-50%"
+            productDiscountLabel="New"
+            productName="Pineapple"
+            productBrand="Cool Brand"
+            productCurrentPrice="$19.99"
+            productOriginalPrice="$39.99"
+          />
 
-              <div className="absolute bottom-2 left-0 flex gap-2">
-                <span className="rounded-r-lg bg-red-500 px-3 py-1.5 text-sm font-semibold uppercase tracking-wider text-white">
-                  -50%
-                </span>
-                <span className="rounded-lg bg-white px-3 py-1.5 text-sm font-bold uppercase tracking-wider text-gray-800">
-                  New
-                </span>
-              </div>
-            </a>
-
-            <div className="flex items-start justify-between gap-2 px-2">
-              <div className="flex flex-col">
-                <a
-                  href="#"
-                  className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
-                >
-                  Pineapple
-                </a>
-                <span className="text-gray-500">by Fancy Brand</span>
-              </div>
-
-              <div className="flex flex-col items-end">
-                <span className="font-bold text-gray-600 lg:text-lg">
-                  $19.99
-                </span>
-                <span className="text-sm text-red-500 line-through">
-                  $39.99
-                </span>
-              </div>
-            </div>
-          </div>
           <div>
             <a
               href="#"
