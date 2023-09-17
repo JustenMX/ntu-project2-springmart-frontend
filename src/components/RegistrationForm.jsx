52254afdd1979ac3b11b6dcfd4d78b83821ffcee
Justen Manni <me@justenmanni.dev>
2023-09-17 15:41:56 +0800

    Update Yup validation schema for Registration Form component

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -54,11 +54,13 @@ function RegistrationForm() {
     validationSchema: Yup.object({
       firstName: Yup.string()
         .max(15, "Must be 15 characters or less")
+        .matches(/^[A-Za-z\s]+$/, "Only letters and spaces are allowed")
         .lowercase()
         .trim()
         .required("Required"),
       lastName: Yup.string()
         .max(20, "Must be 20 characters or less")
+        .matches(/^[A-Za-z\s]+$/, "Only letters and spaces are allowed")
         .lowercase()
         .trim()
         .required("Required"),
